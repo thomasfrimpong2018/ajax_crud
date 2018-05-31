6245eb6b9ea819d9e2ee2443de463a86d2fdc62d
Thomas Frimpong <https://github.com/thomasfrimpong2018>
2018-05-31 18:05:42 +0000

    Initial Commit

---
+++ database/migrations/2018_05_31_165647_create_members-table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('age');
+            $table->string('address');
+            $table->string('email')->unique();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('members');
+    }
+}
