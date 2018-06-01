5b314b4d41c5bd0c5d652144836bceafd2cbb450
Thomas Frimpong <tfrimpong1987@gmail.com>
2018-06-01 16:00:35 +0000

    table created to update member data

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -53,4 +53,43 @@
 </table>
 
 
+<!--Update data-->
+<table>
+ <th>Update</th>
+ <tr>
+  <td>Select Id</td>
+  <td>
+    <select name="upid" id="upid">
+     @foreach($members as $value)
+    <option value="{{$value->id}}">{{$value->id}}</option>
+     @endforeach
+    </select>
+  </td>
+ </tr>
+ <tr>
+  <td>Name:</td>
+  <td><input type="text" name="upname"/></td>
+ </tr>
+ <tr>
+  <td>Age:</td>
+  <td><input type="text" name="upage"/></td>
+ </tr>
+ <tr>
+  <td>Address:</td>
+  <td><input type="text" name="upaddress"/></td>
+ </tr>
+ <tr>
+  <td>Email:</td>
+  <td><input type="text" name="upemail"/></td>
+ </tr>
+ <tr>
+   <td colspan="2"><button type="submit" id="Update"  >Update</td>
+ </tr>
+
+
+
+
+</table>
+
+
 @endsection
