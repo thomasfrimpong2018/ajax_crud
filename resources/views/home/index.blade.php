210cc9bb97c70a62e22f0fcc93352358feacfc03
Thomas Frimpong <tfrimpong1987@gmail.com>
2018-06-06 12:47:24 +0000

    modifications to main page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -51,12 +51,12 @@
 
 
 </table>
-{csrf_field()}  //token generation
+{{ csrf_field() }}
 
 
 <!--Update data-->
 <table>
- <th>Update</th>
+ <th colspan="2" >Update</th>
  <tr>
   <td>Select Id</td>
   <td>
@@ -93,7 +93,7 @@
 </table>
 <!--deleting data-->
 <table>
- <th>Delete</th>
+ <th colspan="2" >Delete</th>
  <tr>
   <td>Select Id:</td>
   <td>
@@ -114,69 +114,6 @@
 
 </table>
 <!--Ajax section-->
-<script type="text/script">
- //for ajax insert
-$(#insert).click(function(){
-$.ajax({
-  type:"post",
-  url:"insert",
-  data:{
-    '_token':$(input[name=_token]).val(),
-    'name':$(input[name=name]).val(),
-    'age':$(input[name=age]).val(),
-    'address':$(input[name=address]).val(),
-    'email':$(input[name=email]).val()
-
-  },
-  success:function(data){
-    window.location.reload()
-
-  }
-})
-});
-
-//for updating Data
-$(#update).click(function(){
-$.ajax({
-  type:"post",
-  url:"update",
-  data:{
-    '_token':$(input[name=_token]).val(),
-    'id':$(#upid).val(),
-    'name':$(input[name=upname]).val(),
-    'age':$(input[name=upage]).val(),
-    'address':$(input[name=upaddress]).val(),
-    'email':$(input[name=upemail]).val(),
-
-  },
-  success:function(data){
-    window.location.reload()
-
-  }
-})
-});
-
-//deleting Data
-$(#delete).click(function(){
-$.ajax({
-  type:"post",
-  url:"delete",
-  data:{
-    '_token':$(input[name=_token]).val(),
-    'id':$(#deleteid).val(),
-
-  },
-  success:function(data){
-    window.location.reload()
-
-  }
-})
-});
-
-
-
-
-</script>
 
 
 
